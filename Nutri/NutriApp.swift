0f1bc0410501617682e4ac7e4afba3e0c3172037
Drumilsp <st.drumilsp@gmail.com>
2026-07-10 23:29:51 +0530

    Initial Commit

---
+++ Nutri/NutriApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriApp.swift
+//  Nutri
+//
+//  Created by Drumil Patil on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct NutriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutriTests/NutriTests.swift
@@ -0,0 +1,19 @@
+//
+//  NutriTests.swift
+//  NutriTests
+//
+//  Created by Drumil Patil on 10/07/26.
+//
+
+import Testing
+@testable import Nutri
+
+struct NutriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
